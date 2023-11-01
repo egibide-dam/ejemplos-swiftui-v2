7d47b6aaee9f0d496f9ce1e49f150bafb89e1f50
Inés Larrañaga <ineslafdez@gmail.com>
2023-11-01 08:05:24 +0100

    Initial Commit

---
+++ ejemplos-swuiftui/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ejemplos-swuiftui
+//
+//  Created by Inés Larrañaga Fernández de Pinedo on 1/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ejemplos-swuiftui/ejemplos_swuiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ejemplos_swuiftuiApp.swift
+//  ejemplos-swuiftui
+//
+//  Created by Inés Larrañaga Fernández de Pinedo on 1/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ejemplos_swuiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
